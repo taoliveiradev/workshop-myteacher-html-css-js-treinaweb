35c5c278f50bfae8bc5224797ec206b93db90e31
Thais Oliveira <65503736+taoliveiradev@users.noreply.github.com>
2024-05-30 21:07:06 -0300

    Catálogo de Professores
    Projeto Prático - HTML CSS JS - Treinaweb

---
+++ README.md
@@ -0,0 +1,40 @@
+<h2> Catálogo de Professores Particulares </h2>
+<h3>HTML, CSS e JavaScript</h3>
+
+<p>
+  <img src="assets/print/screencapture-127-0-0-1-5500-workshop-myteacher-html-css-js-treinaweb-index-html-2024-05-30-20_50_17.png" alt="Print do projeto">
+</p>
+
+<p>Este é o código-fonte da Landing Page "Catálogo de Professores", desenvolvido durante o projeto prático da Treinaweb. </p>
+
+<h3>Tecnologias utilizadas:</h3>
+
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Javascript</li>
+</ul>
+
+<h3>Destaques do projeto:</h3>
+
+<ul>
+  <li><b>Layout responsivo:</b> O site se adapta a diferentes tamanhos de tela, priorizando a experiência em dispositivos móveis. </li>
+  <li><b>Abordagem:</b> A Landing Page permitirá ao usuário informar a quantidade de aulas que deseja e calcular o valor total a pagar para o professor.</li>
+</ul>
+
+<h3> Acesse o projeto: </h3>
+<a href="https://taoliveira.com.br/workshop-myteacher-html-css-js-treinaweb"> Ver Projeto</a> 
+
+<h3>Aprendizados:</h3>
+
+<p>Este projeto me permitiu solidificar meus conhecimentos em:</p>
+
+<ul>
+  <li>Desenvolvimento web com HTML, CSS e Javascript;</li>
+  <li>Responsividade para diferentes tamanhos de tela;</li>
+  <li>Uso de Flexbox;</li>
+  <li>Propriedades e seletores CSS;</li>
+  <li>Sintaxe básica da linguagem JavaScript.</li>
+</ul>
+
+<p>Fique à vontade para explorar o código e me contatar caso tenha alguma dúvida ou sugestão! 😊</p>
---
+++ assets/js/tips/tips.js
@@ -0,0 +1,72 @@
+// // Operações matemáticas
+
+// let num1 = 10;
+// let num2 = 5;
+
+// console.log('num1 = '+ num1);
+// console.log('num2 = '+ num2);
+
+// // Soma
+// console.log('Soma');
+// console.log(num1 + num2);
+
+// // Subtração
+// console.log('Subtração');
+// console.log(num1 - num2);
+
+// // Multiplicação
+// console.log('Multiplicação');
+// console.log(num1 * num2);
+
+// // Divisão
+// console.log('Divisão');
+// console.log(num1 / num2);
+
+// // Módulo
+// console.log('Módulo');
+// console.log(num1 % num2);
+
+// // Arrays
+
+// let notas = [10, 8.9, 7.3, 8.5];
+// console.log(notas);
+// console.log('Nota na posição 3' + " = " + (notas[3]));
+// console.log('Nota na posição 10' + " = " + (notas[10]) + " = " + "Não definido");
+
+// notas[0] = 8.5;
+// console.log(notas);
+
+// notas.push(9.1);
+// console.log(notas);
+
+// Laços de repetição
+// let notas = [10, 8.9, 7.3, 8.5];
+
+// FOR tradicional
+// for(let i = 0; i < 4; i++) {
+//   console.log(notas[i]);
+// }
+// console.log('Terminou o "for"');
+
+// FOR OF
+// for (let nota of notas) {
+//   console.log(nota);
+// }
+// console.log('Terminou o "FOR - OF"');
+
+// // Objetos Javascripts
+// let aluno = {
+//   nome: "Thais Oliveira",
+//   idade: 42,
+//   notas: [10, 8.9, 7.3, 8.5],
+//   contato: {
+//     telefone: "(62) 9.8433-1149",
+//     email: "contato@taoliveira.com.br"
+//   }
+// }
+
+// console.log(aluno);
+// console.log(aluno.nome);
+// console.log(aluno.notas[0]);
+// console.log(aluno.contato.email);
+
